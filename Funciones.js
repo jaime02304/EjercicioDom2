f229a2000943233e454bdf45f933b07b6a12e4c0
DESKTOP-RIMIB5Q\jpribio <jaime.p.r023@gmail.com>
2024-10-04 09:47:08 +0200

    seguir en casa

---
+++ Funciones.js
@@ -0,0 +1,12 @@
+/*const articulos = [
+    {id: 0, name: 'Ratón multiusos', color: 'black', pvp: 15},
+    {id: 1, name: 'Monitor HV 3000', color: 'black', pvp: 180},
+    {id: 2, name: 'Teclado mecánico RGB', color: 'white', pvp: 75},
+    {id: 3, name: 'Webcam HD 1080p', color: 'silver', pvp: 50},
+    {id: 4, name: 'Auriculares inalámbricos', color: 'blue', pvp: 60},
+    {id: 5, name: 'Tablet 10 pulgadas', color: 'black', pvp: 200},
+    {id: 6, name: 'Docking station USB-C', color: 'gray', pvp: 100},
+    {id: 7, name: 'Altavoces Bluetooth', color: 'red', pvp: 40},
+    {id: 8, name: 'Cámara DSLR', color: 'black', pvp: 800},
+    {id: 9, name: 'Proyector portátil', color: 'white', pvp: 300},
+];*/
